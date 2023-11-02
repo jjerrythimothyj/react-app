63a8e8274a9ed0b9353852643b4e088239d62ac7
Jerry Thimothy <jerry.thimothy@builder.ai>
2023-11-03 00:14:06 +0400

    #1 base code

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+  },
+};
