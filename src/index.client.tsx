7618d1bc8d3aaf4e8bc0f1a9ef092ab261f20b17
Mona Lisa <jerry.thimothy@builder.ai>
2023-11-21 23:37:12 +0400

    added index client

---
+++ src/index.client.tsx
@@ -0,0 +1,13 @@
+import * as FEAAS from "@sitecore-feaas/clientside/react";
+export default (props: any) => {
+  debugger;
+  return FEAAS.ExternalComponent(props);
+};
+
+import './components/Input/Input';
+import './components/Button/Button';
+import './components/Text/Text';
+import './components/Card/Card';
+import './components/Carousel/Carousel'
+import './components/Mosaic/Mosaic'
+import './components/ExampleClientsideComponent/ExampleClientsideComponent';
